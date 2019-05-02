9c527eb94939a500eabe70e9cf11bb62f478eb8b
Olivier71 <olivier.nesme@gmail.com>
2019-05-02 18:29:17 +0200

    Ajout d'une condition pour l'année bissextile

--- Bissextile.py
+++ Bissextile.py
@@ -6,7 +6,7 @@
 annee = input("Saisissez une année : ") # On attend que l'utilisateur fournisse l'année qu'il désire tester
 annee = int(annee) # Risque d'erreur si l'utilisateur n'a pas saisi un nombre
 
-if annee % 400 == 0 or (annee % 4 == 0 ):
+if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
     print("L'année saisie est bissextile.")
 else:
     print("L'année saisie n'est pas bissextile.")
